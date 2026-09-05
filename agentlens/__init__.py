ec77f8896cfc4e159a2ae1646db9fdca9f8fee7f
dhananjai <dhananjaiyadav2006@gmail.com>
2026-09-05 16:45:36 +0530

    build: bump version to 0.1.1

--- agentlens/__init__.py
+++ agentlens/__init__.py
@@ -29,7 +29,7 @@ from __future__ import annotations
 
 from agentlens.core import AgentLens, AgentLensError, RunLifecycleError, TraceStateError
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "__version__",
